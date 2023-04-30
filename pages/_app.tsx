f72f25b21d8321dcb8b78c23ba303730f4a25ead
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-04-30 06:40:22 +0000

    make a contact form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import AppContainer from "../components/AppContainer";
 import type { AppProps } from 'next/app'
+import theme from "../config/theme";
 import { useState, useEffect } from 'react';
 import SplashScreen from '../components/SplashScreen';
 
@@ -7,10 +10,18 @@ export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 10000);
+    const timer = setTimeout(() => setLoading(false), 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  return loading ? <SplashScreen /> : <Component {...pageProps} />
+  return loading ? (
+    <SplashScreen />
+  ) : (
+    <ChakraProvider theme={theme}>
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </ChakraProvider>
+  );
 }
