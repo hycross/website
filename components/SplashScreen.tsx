3b6dc40226bc5efdf754f4457bba94c7f5c8092c
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-04-30 06:22:55 +0000

    make loader look better

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -41,7 +41,11 @@ const SplashScreen = () => {
           height={37}
         />
       </div>
-      <div className={styles.spinner}></div>
+      <div className={styles.loader}>
+        <div className={styles.box1}></div>
+        <div className={styles.box2}></div>
+        <div className={styles.box3}></div>
+      </div>
     </main>
   );
 };
