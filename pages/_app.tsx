fb3b3b2e6c5dd14f3b132dc334622b91d774c36b
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-04-30 06:02:20 +0000

    create a next.js website

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react';
+import SplashScreen from '../components/SplashScreen';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 22000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading ? <SplashScreen /> : <Component {...pageProps} />
+}
